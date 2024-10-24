b5f1035cc676c97c9a73f7657d3343646c9e2645
abhinandan202004 <126962453+abhinandan202004@users.noreply.github.com>
2024-10-25 01:59:06 +0530

    Bcrpyt used for hashing feature added

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,48 +1,51 @@
+import bcrypt from "bcryptjs";
 import User from "../models/user.model.js";
 
 export const signup = async (req, res) => {
   try {
     const { fullName, username, password, confirmPassword, gender } = req.body;
-    if(password !== confirmPassword){
+    if (password !== confirmPassword) {
       return res.status(400).json({
-        error :"Passwords don't match"
-      })
+        error: "Passwords don't match",
+      });
     }
-    const user = await User.findOne({username});
+    const user = await User.findOne({ username });
 
-    if(user){
+    if (user) {
       return res.status(400).json({
-        error:"User Name Already exists"
-      })
+        error: "User Name Already exists",
+      });
     }
 
     //password hashing
-    const boyProfilePic = `https://avatar.iran.liara.run/public/boy?username=${username}`
-    const girlProfilePic = `https://avatar.iran.liara.run/public/boy?username=${username}`
+
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    const boyProfilePic = `https://avatar.iran.liara.run/public/boy?username=${username}`;
+    const girlProfilePic = `https://avatar.iran.liara.run/public/boy?username=${username}`;
 
     const newUser = new User({
       fullName,
       username,
-      password,
+      password:hashedPassword,
       gender,
-      profilePic : gender === "male" ? boyProfilePic : girlProfilePic
-    })
+      profilePic: gender === "male" ? boyProfilePic : girlProfilePic,
+    });
     await newUser.save();
     res.status(201).json({
       _id: newUser._id,
       fullName: newUser.fullName,
       username: newUser.username,
-      profilePic: newUser.profilePic
-
-    })
+      profilePic: newUser.profilePic,
+    });
   } catch (error) {
-    console.log("error in signup Controller", error.message)
+    console.log("error in signup Controller", error.message);
     res.status(500).json({
-      error:"Inetrnal server error "
-    })
+      error: "Inetrnal server error ",
+    });
   }
 };
-
 
 export const logout = (req, res) => {
   res.send("user logged Out");
