df8399434a9e1128e5b8846fc419364e744cd991
abhinandan202004 <126962453+abhinandan202004@users.noreply.github.com>
2024-10-26 01:33:13 +0530

    Messagemodel created

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -52,16 +52,55 @@ export const signup = async (req, res) => {
   }catch (error) {
     console.log("error in signup Controller", error.message);
     res.status(500).json({
-      error: "Inetrnal server error ",
+      error: "Internal server error ",
     });
   }
 };
 
 export const logout = (req, res) => {
-  res.send("user logged Out");
-  console.log("SignupUser");
+  try {
+    res.cookie("jwt",{maxAge:0})
+    res.status(200).json({message: "Logged out Succesfully"});
+  } catch (error) {
+    console.error("Error in login Controller:", error.message);
+    res.status(500).json({
+      error: "Internal server error",
+    });
+  }
 };
 
-export const login = (req, res) => {
-  console.log("loginUser");
+export const login = async (req, res) => {
+  try {
+    const { username, password } = req.body;
+    
+    // Check if user exists
+    const user = await User.findOne({ username });
+    if (!user) {
+      return res.status(400).json({ error: "Invalid Username or Password" });
+    }
+
+    // Check if password is correct
+    const isPasswordCorrect = await bcrypt.compare(password, user.password);
+    if (!isPasswordCorrect) {
+      return res.status(400).json({ error: "error" });
+    }
+
+    // Generate token and set cookie
+    generateTokenAndSetCookie(user._id, res);
+
+    // Respond with user details
+    res.status(200).json({
+      _id: user._id,
+      fullName: user.fullName,
+      username: user.username,
+      profilePic: user.profilePic,
+    });
+
+  } catch (error) {
+    console.error("Error in login Controller:", error.message);
+    res.status(500).json({
+      error: "Internal server error",
+    });
+  }
 };
+
